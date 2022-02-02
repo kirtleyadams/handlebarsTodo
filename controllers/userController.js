8a2faf47e51ee5b34c32f73d7cd4bde22f76ba85
Kirtley Adams <hello@kirtleymichelle.com>
2022-02-01 22:02:23 -0500

    added helpers

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+
+const {
+	User
+} = require('../models');
+module.exports = {
+	createUser: async (req, res) => {
+		const { username, email, password } = req.body;
+		if (!username || !email || !password ) {
+			return res.status(400).json({ error: 'You must provide a username, email, and password'});
+		}
+		try {
+			const user = await User.create({
+				username,
+				email,
+				password,
+			});
+			res.json(user);
+		} catch (e) {
+			res.json(e);
+		}
+	},
+//	getting users
+	getAllUsers: async (req, res) => {
+		try {
+			const usersData = await User.findAll({});
+			const users = usersData.map(user => user.get({plain:true}));
+
+			res.render('allUsers', {
+				users,
+				favoriteFood: 'Ice cream',
+			});
+		} catch (e) {
+			res.json(e);
+		}
+	},
+	getUserById: async (req, res) => {
+		try {
+			const userData = await user.findByPk(req.params.userId);
+		const user = userData.get({plain: true});
+		res.render('singleUser',{
+			user
+		});
+		} catch(e) {
+			res.json(e)
+		}
+
+	}
+}
